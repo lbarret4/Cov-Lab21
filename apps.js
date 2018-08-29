fbf3eb0810b15866a44e491a0b1fc163ba6d4b96
lbarret4 <llewellynbar4@gmail.com>
2018-08-29 16:48:28 -0400

    Added several call back functions

--- apps.js
+++ apps.js
@@ -1,9 +1,9 @@
 
 let num1 = messageCharLength();
-
+let words=['pathos','forbearance','andragogy','dreamboat'];
 setTimeout(multiplyByFour,2000);
 
-
+getWords(words);
 
 function messageCharLength(){
     let message = "This is a message";
@@ -13,4 +13,27 @@ function messageCharLength(){
 
 function multiplyByFour(){
     console.log(num1*4);
+}
+
+function getWords(words){
+    words.forEach((word,index) => {
+        let time = index == 0 ? 0: (words.length-index)*1000;
+        setTimeout((words) =>{
+            console.log(words);
+        },time);
+    });
+}
+
+function countdown(num,callback){
+    for(let i = 1;i <= num; i++){
+        if(i != 1){
+            setTimeout(()=>console.log(i),1000)
+        } else {
+            callback();
+        }        
+    }
+}
+
+function done(){
+    console.log('done');
 }
