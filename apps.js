399dce2b56eb33eb613f140bd2e177d2970389b6
lbarret4 <llewellynbar4@gmail.com>
2018-08-29 15:51:26 -0400

    Demonstrate asynchronicity with setTimeout function

---
+++ apps.js
@@ -0,0 +1,16 @@
+
+let num1 = messageCharLength();
+
+setTimeout(multiplyByFour,2000);
+
+
+
+function messageCharLength(){
+    let message = "This is a message";
+    console.log(message);
+    return message.length;
+}
+
+function multiplyByFour(){
+    console.log(num1*4);
+}
